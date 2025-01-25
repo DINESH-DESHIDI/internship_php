49b91a597e9a29ab6a8867db834c734cd0526a04
DINESH-DESHIDI <deshididinesh@gmail.com>
2025-01-25 11:03:20 +0530

    Create script.js

---
+++ dashboards/provider_dashboard/script.js
@@ -0,0 +1,19 @@
+ document.getElementById("request_page").addEventListener("click", function () {
+      window.location.href = "request_page.html"; 
+    });
+
+  document.getElementById("area 1").addEventListener("click", function () {
+      window.location.href = "area_1.html"; 
+    });
+
+  document.getElementById("area 2").addEventListener("click", function () {
+      window.location.href = "area_2.html"; 
+    });
+
+  document.getElementById("area 3").addEventListener("click", function () {
+      window.location.href = "area_3.html"; 
+    });
+
+    document.getElementById("area 4").addEventListener("click", function () {
+      window.location.href = "area_4.html"; 
+    });
