b44fef7e3df37c5847a7239da6bcc2cbc750d60f
DINESH-DESHIDI <deshididinesh@gmail.com>
2025-01-25 11:31:35 +0530

    Create script.js

---
+++ areas/area_3/script.js
@@ -0,0 +1,8 @@
+
+  document.getElementById("new form1").addEventListener("click", function () {
+      window.location.href = "new form1.html"; 
+    });
+
+    document.getElementById("new form2").addEventListener("click", function () {
+      window.location.href = "new form2.html"; 
+    });
