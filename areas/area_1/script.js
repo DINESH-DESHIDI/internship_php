c65e1aae320e2378f70ee27daf9acaf126fdccc6
DINESH-DESHIDI <deshididinesh@gmail.com>
2025-01-25 11:18:45 +0530

    Delete areas/area_1 directory

--- areas/area_1/script.js
+++
@@ -1,8 +0,0 @@
-
-  document.getElementById("new form1").addEventListener("click", function () {
-      window.location.href = "new form1.html"; 
-    });
-
-    document.getElementById("new form2").addEventListener("click", function () {
-      window.location.href = "new form2.html"; 
-    });
